0a5d1fe65666aeabebc99e0e183a6650fcd8e01c
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-10-02 20:31:49 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Info from "./components/Info" 
+import About from "./components/About"
+import Interest from "./components/Interest"
+import Footer from "./components/Footer"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Info/>
+            <About/>
+            <Interest/>
+            <Footer/>
+        </div>
+    )
+}
